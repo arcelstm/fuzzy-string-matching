df3f8bed67aef7f232c7cdcc700129f42272b373
marcel <marcel.stacha@gmail.com>
2023-10-26 11:04:51 +0200

    First push

--- fuzzy.py
+++ fuzzy.py
@@ -1,1 +1,7 @@
-#testtest
+from thefuzz import fuzz
+
+a = "Assassin's Creed III"
+b = "Assassins Creed III"
+
+print(f"Ratio: {fuzz.ratio(a, b)}")
+print(f"Partial Ratio: {fuzz.partial_ratio(a, b)}")
